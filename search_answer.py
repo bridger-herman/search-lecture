f7d6d0b7bc93f9c96fc5c382546cdc2c1d64961d
Bridger Herman <herma582@umn.edu>
2024-05-13 15:36:05 -0500

    Updates for clarity

--- search_answer.py
+++ search_answer.py
@@ -15,23 +15,23 @@ def binary_search(values: list, target_value: int) -> int:
     """Returns the index of value `target_value` in the list `values`. NOTE:
     `values` MUST be sorted!"""
 
-    min_index = 0
-    max_index = len(values) - 1
-    while min_index <= max_index:
+    lo_index = 0
+    hi_index = len(values) - 1
+    while lo_index <= hi_index:
         # cut the range in half
-        current_index = (min_index + max_index) // 2
+        mid_index = (lo_index + hi_index) // 2
 
         # if the target is right in the middle, just return the index!
-        if target_value == values[current_index]:
-            return current_index
+        if target_value == values[mid_index]:
+            return mid_index
         # if the target is "above" the midpoint, move the min_index up
         # "+1" because we already know it's not directly equal to midpoint
-        elif target_value > values[current_index]:
-            min_index = current_index + 1
+        elif target_value > values[mid_index]:
+            lo_index = mid_index + 1
         # if the target is "below" the midpoint, move the max_index down
         # "-1" because we already know it's not directly equal to midpoint
         else:
-            max_index = current_index - 1
+            hi_index = mid_index - 1
 
     return -1
 
@@ -41,31 +41,63 @@ def binary_search_recursive(values: list, target_value: int) -> int:
     binary search"""
     return binary_search_recursive_helper(values, target_value, 0, len(values) - 1, 0)
 
-def binary_search_recursive_helper(values: list, target_value: int, min_index: int, max_index: int) -> int:
+def binary_search_recursive_helper(values: list, target_value: int, lo_index: int, hi_index: int) -> int:
     """Returns the index of value `target_value` in the list `values`. NOTE:
     `values` MUST be sorted! This is basically the same logic as the iterative
     version. We also have two initial conditions: min_index and max_index."""
     # cut the range in half
-    current_index = (min_index + max_index) // 2
+    mid_index = (lo_index + hi_index) // 2
 
     # BASE CASE 1
     # reached the end and didn't find it
-    if min_index > max_index:
+    if lo_index > hi_index:
         return -1
     # BASE CASE 2
     # if the target is right in the middle, just return the index!
-    elif target_value == values[current_index]:
-        return current_index
+    elif target_value == values[mid_index]:
+        return mid_index
     # RECURSIVE OPTION 1
     # if the target is "above" the midpoint, move the min_index up
     # "+1" because we already know it's not directly equal to midpoint
-    elif target_value > values[current_index]:
-        return binary_search_recursive_helper(values, target_value, current_index + 1, max_index)
+    elif target_value > values[mid_index]:
+        return binary_search_recursive_helper(values, target_value, mid_index + 1, hi_index)
     # RECURSIVE OPTION 2
     # if the target is "below" the midpoint, move the max_index down
     # "-1" because we already know it's not directly equal to midpoint
     else:
-        return binary_search_recursive_helper(values, target_value, min_index, current_index - 1)
+        return binary_search_recursive_helper(values, target_value, lo_index, mid_index - 1)
+
+def binary_search_recursive2(values: list, target_value: int) -> int:
+    """Returns the index of value `target_value` in the list `values`. NOTE:
+    `values` MUST be sorted! This is basically the same logic as the iterative
+    version. This one uses list slicing instead."""
+    # cut the range in half
+    mid_index = (len(values) - 1) // 2
+
+    # BASE CASE 1
+    # reached the end and didn't find it
+    if len(values) == 0:
+        return -1
+    # BASE CASE 2
+    # if the target is right in the middle, just return the index!
+    elif target_value == values[mid_index]:
+        return mid_index
+    # RECURSIVE OPTION 1
+    # if the target is "above" the midpoint, move the min_index up
+    # "+1" because we already know it's not directly equal to midpoint
+    elif target_value > values[mid_index]:
+        result = binary_search_recursive2(values[mid_index + 1:], target_value)
+        if result >= 0:
+            # adjust "starting" index, if found
+            return mid_index + 1 + result
+        else:
+            # not found, propagate -1
+            return -1
+    # RECURSIVE OPTION 2
+    # if the target is "below" the midpoint, move the max_index down
+    # "-1" because we already know it's not directly equal to midpoint
+    else:
+        return binary_search_recursive2(values[:mid_index], target_value)
 
 
 
@@ -77,8 +109,8 @@ def main():
     print('should find every item and output indices sequentially, plus 3 items not in the list.')
     for target_value in values + [51, 99, 121]:
         # found_index = linear_search(values, target_value)
-        found_index = binary_search(values, target_value)
-        # found_index = binary_search_recursive(values, target_value)
+        # found_index = binary_search(values, target_value)
+        found_index = binary_search_recursive2(values, target_value)
 
         if found_index >= 0:
             print(f'found {target_value}: index =', found_index)
